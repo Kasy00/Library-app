acfa6b4a176a89de73ffca10a8f9cd3e67f3574f
Kasy00 <kamilsyktus3@gmail.com>
2023-12-05 13:53:04 +0100

    Added listener to buttons when read and not read

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const dialog = document.querySelector('dialog');
 const addBtn = document.getElementById('addBtn');
+const readBtn = document.querySelector(".readBtn");
+const removeBtn = document.querySelector(".removeBtn");
+const card = document.querySelector('.card');
 
 addBtn.addEventListener('click', () =>{
     dialog.showModal();
@@ -10,4 +13,19 @@ dialog.addEventListener('click', e =>{
     if(e.clientX < dialogDimensions.left || e.clientX > dialogDimensions.right || e.clientY < dialogDimensions.top || e.clientY > dialogDimensions.bottom){
         dialog.close();
     }
+});
+
+readBtn.addEventListener('click', () =>{
+    if(readBtn.textContent==="READ"){
+        readBtn.textContent = "NOT READ";
+        readBtn.style.backgroundColor = "red";
+    }
+    else{
+        readBtn.textContent = "READ";
+        readBtn.style.backgroundColor = "green";S
+    }
+});
+
+removeBtn.addEventListener('click', () =>{
+    card.st
 });
